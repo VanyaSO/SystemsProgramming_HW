f6ae6baf6555ad8abed69a20d18b7098bcc97a75
Vanya Ushachov <90464856+VanyaSO@users.noreply.github.com>
2024-09-19 12:34:23 +0300

    added HW_1

---
+++ HW_1/HW_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace HW_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.FileName = "open";
+                process.StartInfo.Arguments = "-a Safari";
+                process.StartInfo.CreateNoWindow = true;
+                process.EnableRaisingEvents = true;
+                process.Exited += (sender, eventArgs) => Console.WriteLine($"Процес завершился с кодом: {process.ExitCode}");
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ HW_1/HW_1_dop_4/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+
+namespace HW_1_dop_4;
+
+class Program
+{
+    private static string[] _menu = new[] { "Выйти", "TextEdit", "Safari", "Telegram", "Google Ghrome", "Своё приложение" };
+    
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Clear();
+            for (int i = 0; i < _menu.Length; i++)
+            {
+                Console.WriteLine($"{i+1}. {_menu[i]}");
+            }
+
+            Console.WriteLine("\nВыберите програму для запуска");
+            if (Int32.TryParse(Console.ReadLine(), out int app) && app > 0 && app <= _menu.Length)
+            {
+                app--;  // Преобразуем в индекс массива
+    
+                if (app == 0)
+                {
+                    // Выход из программы
+                    break;
+                } 
+                else if (app == _menu.Length - 1)
+                {
+                    // Пользовательское приложение
+                    Console.WriteLine("Введите путь к своему приложению:");
+                    string custPath = Console.ReadLine();
+                    StartCustApp(custPath);  // Метод для запуска пользовательского приложения
+                }
+                else
+                {
+                    // Стандартные приложения из меню
+                    StartApp(_menu[app]);
+                }
+    
+                Console.WriteLine("Процесс запущен");   
+            }
+            else
+                Console.WriteLine("Выберите корректный вариант");
+            
+            Console.WriteLine("Нажите любой кнопку чтобы продолжить...");
+            Console.ReadLine();
+        }
+    }
+
+    static void StartApp(string app)
+    {
+        using (Process process = new Process())
+        {
+            process.StartInfo.FileName = "open";
+            process.StartInfo.Arguments = $"-a {app}";
+            process.Start();
+        }
+    }
+    
+    static void StartCustApp(string app)
+    {
+        using (Process process = new Process())
+        {
+            process.StartInfo.FileName = "open";
+            process.StartInfo.Arguments = $"{app}";
+            process.Start();
+        }
+    }
+}
