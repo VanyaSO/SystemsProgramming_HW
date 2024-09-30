2f34e5e55edd566024467757bf56c18ce64f4139
Vanya Ushachov <ushachov.ivan@armar.solutions>
2024-09-30 10:02:06 +0300

    add hw_5

---
+++ HW_5/HW_5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+
+namespace HW_5;
+
+class Program
+{
+    // У вас есть список URL-адресов, которые нужно загрузить параллельно (загрузить содержимое страницы),
+    // используя несколько потоков на C#. В программе должна быть возможность отменить операцию, если она
+    // занимает слишком много времени или если пользователь решит ее отменить.
+    // Напишите программу, которая загружает файлы, используя TPL и CancellationToken.
+    static void Main(string[] args)
+    {
+        List<string> urls = new List<string>
+        {
+            "https://od.itstep.org/",
+            "https://www.microsoft.com/uk-ua/"
+        };
+        CancellationTokenSource cls = new CancellationTokenSource();
+
+        Task[] tasks = new Task[urls.Count];
+
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            tasks[i] = Task.Factory.StartNew(() =>
+            {
+                while (urls.Count > 0 && !cls.Token.IsCancellationRequested)
+                {
+                    string? url = null;
+                    int count = 1;
+                    lock (urls)
+                    {
+                        count += 1;
+                        url = urls[0];
+                        urls.RemoveAt(0);
+                    }
+
+                    if (url != null)
+                    {
+                        try
+                        {
+                            WebClient client = new WebClient();
+                            client.DownloadFile(url, $"site{count}.txt");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+                }
+            }, cls.Token);
+        }
+
+        Console.WriteLine("Stop load ?");
+        Console.ReadKey();
+        
+        cls.Cancel();
+        Task.WaitAll(tasks);
+    }
+}
---
+++ HW_5/HW_5_dop_1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace HW_5_dop_1;
+
+class Program
+{
+    // Создайте приложение для работы с массивом: 
+    //
+    // ■ Удаление из массива повторяющихся значений; 
+    // ■ Сортировка массива (стартует после удаления дублей); 
+    // ■ Бинарный поиск некоторого значения (стартует после сортировки). 
+    //
+    // Используйте «Continuation Tasks» для решения поставленной задачи.
+    
+    static void Main(string[] args)
+    {
+        List<int> arr = new List<int>{ 1, 2, 7, 3, 8, 2, 75, 34, 7, 2, 3, 7, 8, 0, 7, 45, 3, 8, 5 };
+        
+        Task<List<int>> distinct = Task.Run(() => arr.Distinct().ToList());
+        
+        Task<List<int>> sort = distinct.ContinueWith(task =>
+        {
+            var array = task.Result;
+            array.Sort();
+            return array;
+        });
+        
+        Task<int> search = sort.ContinueWith(task =>
+        {
+            var array = task.Result;
+            return array.BinarySearch(5);
+        });
+
+        Console.WriteLine();
+        Console.WriteLine(search.Result);
+    }
+}
