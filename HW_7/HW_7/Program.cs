9801a6495cca45a41a97d6b1ba4e323ab1d49685
Vanya Ushachov <ushachovg324@gmail.com>
2024-10-08 20:46:13 +0300

    add hw_7

---
+++ HW_7/HW_7/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace HW_7;
+
+// Необходимо загрузить информацию о пользователе с сервера или локальной базы данных, используя асинхронный метод.
+// Если во время загрузки возникает ошибка (например, сервер не отвечает), нужно обработать эту ошибку и уведомить пользователя о проблеме.
+// Выполните имитацию зависания сервера и отмените операцию, если запрос к серверу выполняется более 10 секунд.
+    
+class Program
+{
+    static async Task Main()
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+            db.SaveChanges();
+
+        }
+
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            db.Users.AddRange(new List<User>
+            {
+                new User {Name = "Alex"},
+                new User {Name = "Misha"}
+            });
+        }
+
+        User? user = await GetUserAsync();
+        if (user != null)
+        {
+            Console.WriteLine(user.Name);
+        }
+
+    }
+
+    static async Task<User?> GetUserAsync()
+    {
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(10000);
+        
+        
+        Task<User?> result = Task.Run(async () =>
+        {
+            await Task.Delay(15000);
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                return await db.Users.FirstOrDefaultAsync();
+            }
+        });
+
+        if (await Task.WhenAny(result, Task.Delay(10000, cancellationTokenSource.Token)) == result) return await result;
+
+        throw new OperationCanceledException("Error");
+    }
+};
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {        
+        optionsBuilder.UseSqlServer("Server=localhost;Database=HW;User=sa;Password=admin@Admin87457;TrustServerCertificate=True;");
+    }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ HW_7/HW_7_dop_1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace HW_7_dop_1;
+
+// Обработать несколько исключений в программе. Создайте асинхронный метод, который принимает массив строк.
+// Вызовите этот метод минимум три раза, передав разные имена людей. В методе, если есть повторяющиеся имена, генерируйте ошибку.
+// Если повторений нет, выведите имена в консоль. После выполнения всех вызовов выведите в консоль все возникшие ошибки.
+    
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var tasks = new List<Task>();
+
+        try
+        {
+            tasks.Add(HasDuplicate(new List<string> { "Alisa", "Ivan", "Ivan" }));
+            tasks.Add(HasDuplicate(new List<string> { "Sasha", "Ivan", "Ivan" }));
+            tasks.Add(HasDuplicate(new List<string> { "Anastasia", "Anastasia", "Kate" }));
+            await Task.WhenAll(tasks);
+        }
+        catch
+        {
+            foreach (var task in tasks)
+            {
+                if (task.IsFaulted)
+                {
+                    foreach (var innerException in task.Exception.InnerExceptions)
+                    {
+                        Console.WriteLine(innerException.Message);
+                    }
+                }
+            }
+        }
+    }
+
+    static async Task HasDuplicate(List<string> arr)
+    {
+        List<string> duplicates = new List<string>();
+        
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < arr.Count; i++)
+            {
+                for (int j = i + 1; j < arr.Count; j++)
+                {
+                    if (arr[i] == arr[j])
+                        duplicates.Add(arr[i]);
+                }
+                if (!duplicates.Contains(arr[i]))
+                    Console.WriteLine(arr[i]);
+            }
+        });
+        
+        if (duplicates.Any())
+            throw new AggregateException(duplicates.
+                Select(name => new Exception($"Дублируется имя {name}"))
+                .ToList());
+    }
+}
